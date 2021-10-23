c5f88ed35f548856ae4b62c6257359f705670daa
Janis Boegershausen (Pc) <janis.boegershausen@gmx.net>
2021-10-23 13:54:01 +0200

    Added a simulated graph as a header

--- Page.js
+++ Page.js
@@ -1,21 +1,28 @@
 var particles = [];
+var time = 10;
 
 var gravity = new Vector(0, 0.1);
 
 function setup() {
   // Create canvas and set its parent
-  var c = createCanvas();
+  var c = createCanvas(width, height);
   var parent = document.getElementById("header-canvas-parent");
   c.parent(parent);
+  fitCanvasToParent();
 
   // Create particles
-  particles.push(new Particle());
+  var count = 30;
+  for (let i = 0; i < count; i++) {
+    particles.push(new Particle(Math.random() * width, Math.random() * height, Math.floor(RandomInRange(0, count))));
+  }
 }
 
 function draw() {
+  time += deltaTime / 1000;
+
   fitCanvasToParent();
   // Clear
-  background(48, 50, 61)
+  background(48, 50, 61);
 
   // Update all particles
   particles.forEach((p) => {
@@ -30,15 +37,35 @@ function fitCanvasToParent() {
 }
 
 class Particle {
-  constructor() {
-    this.position = new Vector(100, 100);
-    this.velocity = new Vector(0, 2);
+  constructor(x, y, indexOfConnected) {
+    this.position = new Vector(x, y);
+    this.velocity = new Vector(0, 0);
+    this.color = new Color(200, 200, 200);
+    this.seedA = random();
+    this.seedB = random();
+    this.indexOfConnected = indexOfConnected;
+    this.lineProgress = 0.5;
   }
 
   Draw() {
-    this.velocity = Vector.Add(this.velocity, gravity)
-    this.position = Vector.Add(this.position, this.velocity);
-    fill(255);
-    rect(this.position.x, this.position.y, 50, 50);
+    this.velocity = Vector.Add(this.velocity, Vector.Scale(new Vector(RandomInRange(-1, 1), RandomInRange(-1, 1)), 0.15));
+    this.velocity = this.velocity.normalized();
+    this.position = Vector.Wrap(Vector.Add(this.position, this.velocity), new Vector(0, 0), new Vector(width, height));
+
+    var d = Vector.Distance(this.position, particles[this.indexOfConnected].position);
+    if (d < 200 && d > 5 && particles[particles[this.indexOfConnected].indexOfConnected] != this) {
+      this.lineProgress = Math.min(this.lineProgress + (deltaTime / 1000 * 5), 1);
+      var direction = Vector.Sub(particles[this.indexOfConnected].position, this.position).normalized();
+      this.velocity = Vector.Add(this.velocity, Vector.Scale(direction, Remap(d, 5, 200, -0.25, 0.05)));
+      stroke(150);
+      line(this.position.x, this.position.y, this.position.x + direction.x * this.lineProgress * d, this.position.y + direction.y * this.lineProgress * d);
+    } else {
+      this.indexOfConnected = Math.floor(RandomInRange(0, particles.length));
+      this.lineProgress = 0;
+    }
+
+    noStroke();
+    fill(this.color.r, this.color.g, this.color.b);
+    circle(this.position.x, this.position.y, 5, 5);
   }
 }
